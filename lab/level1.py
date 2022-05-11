b432c6f358e2e2b198efd256bdb09d2613e7f0c9
nohandsome <1797695463@qq.com>
2022-05-11 11:19:12 +0800

    format fix

--- lab/level1.py
+++ lab/level1.py
@@ -1,18 +1,23 @@
 # level1.1 code
 # y = x^2 - 2*x + 1
+
 from math import sqrt
+
 
 def solve(y):
     res = []
+    # special case
     if y < 0:
         return res
     if y == 0:
         res.append(1)
         return res
+    # normal case
     a = sqrt(y)
     res.append(a+1)
     res.append(-a+1)
     return res
+
 
 y = int(input("y:"))
 res = []
