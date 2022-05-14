501924a67a924328d4150330b5692cfcb8498313
nohandsome <1797695463@qq.com>
2022-05-14 11:13:01 +0800

    add ocr code

---
+++ lab/level2.14.py
@@ -0,0 +1,29 @@
+# level2.14 code
+import easyocr
+import os
+
+dic_path = "./images/"
+save_path = "./images_res/"
+# 计算该目录下所有图片
+# 实现自动化操作
+image_namelist = os.listdir(dic_path)
+
+
+# this needs to run only once to load the model into memory
+# 本人穷，无gpu
+reader = easyocr.Reader(['ch_sim', 'en'], gpu=False)
+i = 1
+
+for image in image_namelist:
+    s1 = dic_path + image
+    # 使用model来计算结果
+    result = reader.readtext(s1, detail=0)
+    # 将结果写入存放结果的目录
+    s2 = save_path + image + '.txt'
+    with open(s2, "w") as f:
+        res = " ".join(result)
+        f.write(res)
+    print("第" + str(i) + "个计算完成.")
+    i = i + 1
+
+print('All Done!')
