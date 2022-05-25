b8cea209587bfafa00c2e7901f92002f202db179
nohandsome <1797695463@qq.com>
2022-05-25 10:19:07 +0800

    add clean fun

--- lab/level5/clean.py
+++ lab/level5/clean.py
@@ -1,28 +1,19 @@
 import os
-import shutil
 
-try:
-    os.remove('./images_sort/ironman/3.2.png')
-    os.remove('./images_sort/ironman/3.3.png')
-    os.remove('./images_sort/ironman/3.4.png')
-    os.remove('./images_sort/spiderman1/1.2.png')
-    os.remove('./images_sort/spiderman1/1.4.png')
-    os.remove('./images_sort/spiderman1/1.3.png')
-    os.remove('./images_sort/spiderman1/1.1.png')
-    os.remove('./images_sort/long/4.1.png')
-    os.remove('./images_sort/long/4.2.png')
-    os.remove('./images_sort/long/4.3.png')
-    os.remove('./images_sort/long/4.4.png')
-    os.remove('./images_sort/spiderman2/2.1.png')
-    os.remove('./images_sort/spiderman2/2.2.png')
-    os.remove('./images_sort/spiderman2/2.3.png')
-    os.remove('./images_sort/spiderman2/2.4.png')
-    os.remove('./images_sort/spiderman2/2.5.png')
-except:
-    try:
-        shutil.rmtree('./auto_sort')
-    except:
-        pass
+def cleantest(rm_dir):
+    # 将rm_dir文件夹中的所有一级目录里的图片删除至一张
+    # 便于测试前面的功能，不必反复手动删除
 
+    # 得到一级目录
+    f_dir = []
+    for root, dirs, files in os.walk(rm_dir, topdown=False):
+        for name in dirs:
+            f_dir.append(os.path.join(root, name))
 
+    for dir in f_dir:
+        files = os.listdir(dir)
+        if len(files) >= 2:
+            for i in range (1, len(files)):
+                os.remove(os.path.join(dir, files[i]))
 
+    
--- lab/level5/main.py
+++ lab/level5/main.py
@@ -1,6 +1,7 @@
 import recognize
 import sys
 import time
+import clean
 
 start_time = time.time()
 
@@ -8,7 +9,9 @@ if sys.argv[1] == '-sort_k':
     recognize.sort_by_known(sys.argv[2], sys.argv[3])
 elif sys.argv[1] == '-auto_sort':
     recognize.auto_sort(sys.argv[2], sys.argv[3])
-
+elif sys.argv[1] == '-clean':
+    clean.cleantest(sys.argv[2])
+    
 
 end_time = time.time()
 print("Tatal time {:.2f}s".format(end_time - start_time))
