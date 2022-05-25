647cad82ce82d799040a1a51cab392317df2774d
nohandsome <1797695463@qq.com>
2022-05-25 10:19:32 +0800

    add clean fun

--- lab/level5/clean.py
+++ lab/level5/clean.py
@@ -1,5 +1,6 @@
 import os
 
+
 def cleantest(rm_dir):
     # 将rm_dir文件夹中的所有一级目录里的图片删除至一张
     # 便于测试前面的功能，不必反复手动删除
@@ -13,7 +14,5 @@ def cleantest(rm_dir):
     for dir in f_dir:
         files = os.listdir(dir)
         if len(files) >= 2:
-            for i in range (1, len(files)):
+            for i in range(1, len(files)):
                 os.remove(os.path.join(dir, files[i]))
-
-    
